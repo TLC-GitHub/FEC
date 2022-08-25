a0ba3d5cddfe26e4367f227d33782b4e23c88f9e
Siming Yum <zhuandshi@gmail.com>
2022-08-25 13:48:54 -0700

    modify get request and add post and put request to api

--- server/app.js
+++ server/app.js
@@ -12,23 +12,64 @@ app.use(express.json());
 //----------for testing only, will delete after testing----------------//
 let queryParams = {
   product_id: 65656,
-  page: 1,
-  count: 10
+  // page: 1,
+  // count: 10
   // sort: "newest"
+  rating: 4,
+  summary: "summary of my review",
+  body: "this is a review for testing testing testing",
+  recommend: true,
+  name: "testing testing testing",
+  email: "testing@gmail.com",
+  photos: [],
+  characteristics: {}
+}
+
+let bodyParams = {
+  product_id: 65656,
+  // body: "I am providing testing answer here here here!!!!!!!",
+  // name: "answer",
+  // email: "answer@gmail.com"
+  rating: 4,
+  summary: "summary of my review",
+  body: "this is a review for testing testing testing",
+  recommend: true,
+  name: "testing testing testing",
+  email: "testing@gmail.com",
+  photos: [],
+  characteristics: {}
 }
 
 // let pathVariable = 65656 //product_id
-let pathVariable = 574069 //question_id
+// let pathVariable = 574069 //question_id
+// let pathVariable = 642691 //question_id
+let pathVariable = 5987428 //answer_id
 
-helper.getProductInfo('products', queryParams, '')
-  .then((response) => {
-    console.log("data from api: ", response.data);
-  })
-  .catch((err) => {
-    console.log("something went wrong: ", err);
-  })
+// helper.getInfo('qa/questions', queryParams)
+//   .then((response) => {
+//     console.log("data from api: ", response.data);
+//   })
+//   .catch((err) => {
+//     console.log("something went wrong: ", err);
+//   })
 
+// helper.postInfo('reviews', bodyParams)
+//   .then((response) => {
+//     console.log("data from api: ", response.data);
+//   })
+//   .catch((err) => {
+//     console.log("something went wrong: ", err);
+//   })
+
+  // helper.updateInfo('qa/answers', pathVariable, 'helpful')
+  // .then((response) => {
+  //   console.log("data from api: ", response.data);
+  // })
+  // .catch((err) => {
+  //   console.log("something went wrong: ", err);
+  // })
 // --------------------------------------------------------------------//
+
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
--- server/hrapi.js
+++ server/hrapi.js
@@ -1,27 +1,59 @@
 const axios = require('axios');
 const url = require('url')
 
-let getProductInfo = (widget, queryParams, subCategory='') => {
+let apiLink = "";
 
+let getInfo = (widget, queryParams, subCategory='') => {
   if (typeof queryParams === 'object') {
-
     const params = new url.URLSearchParams(queryParams);
     apiLink = `https://app-hrsei-api.herokuapp.com/api/fec2/rfp/${widget}?${params}`;
-
   } else {
-
     apiLink = `https://app-hrsei-api.herokuapp.com/api/fec2/rfp/${widget}/${queryParams}/${subCategory}`;
-
   }
-
+  console.log(apiLink);
   let options = {
     url: apiLink,
     headers: {
       'Authorization': process.env.API_TOKEN
     }
   };
-
   return axios(options);
 }
 
-module.exports.getProductInfo = getProductInfo;
+let postInfo = (widget, bodyParams, queryParams, subCategory='') => {
+  console.log("------------in the post function------------")
+  if (queryParams === undefined) {
+    console.log("----------i should be here--------------")
+    apiLink = `https://app-hrsei-api.herokuapp.com/api/fec2/rfp/${widget}`;
+  } else {
+    apiLink = `https://app-hrsei-api.herokuapp.com/api/fec2/rfp/${widget}/${queryParams}/${subCategory}`;
+  }
+  console.log(apiLink);
+  console.log(bodyParams);
+  let options = {
+    url: apiLink,
+    method: 'POST',
+    data: bodyParams,
+    headers: {
+      'Authorization': process.env.API_TOKEN
+    }
+  };
+  return axios(options);
+}
+
+let updateInfo = (widget, queryParams, subCategory) => {
+  apiLink = `https://app-hrsei-api.herokuapp.com/api/fec2/rfp/${widget}/${queryParams}/${subCategory}`;
+  console.log(apiLink);
+  let options = {
+    url: apiLink,
+    method: 'PUT',
+    headers: {
+      'Authorization': process.env.API_TOKEN
+    }
+  };
+  return axios(options);
+}
+
+module.exports.getInfo = getInfo;
+module.exports.postInfo = postInfo;
+module.exports.updateInfo = updateInfo;
