2ef33350fb5397fb8b71062cfd98494ce6041e44
Shawn <sro1115@outlook.com>
2022-08-24 17:01:55 -0700

    included QNA components/ .env basic server w/ port 3000

---
+++ server/app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.json());
+
+app.listen(process.env.PORT);
+console.log(`Listening at http://localhost:${process.env.PORT}`);
