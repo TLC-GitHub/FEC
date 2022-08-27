acb7be7087fbd2eadf97fbdc4a2a537f44d9ba4d
Siming Yum <zhuandshi@gmail.com>
2022-08-26 18:30:20 -0700

    implement axios request from client to server to api

---
+++ client/src/RelatedItems/Components/MyOutfit.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import ProductCard from './ProductCard.jsx';
+
+function MyOutfit() {
+
+
+
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  )
+}
+
+export default MyOutfit;
---
+++ client/src/RelatedItems/Components/RelatedProducts.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import ProductCard from './ProductCard.jsx';
+
+// const helper = require('../server/hrapi.js');
+
+function RelatedProducts() {
+
+
+
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  )
+}
+
+export default RelatedProducts;
