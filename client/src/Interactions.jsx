3ecbde0ae62ae5b5dc8308058ab67d5d9070134b
Shawn <sro1115@outlook.com>
2022-09-09 13:20:52 -0700

    interactions fully implemeneted

--- client/src/Interactions.jsx
+++ client/src/Interactions.jsx
@@ -3,22 +3,20 @@ import axios from 'axios';
 
 export const logInteractions = (e) => {
 
-  let element = e.target;
+  let element = e.target.outerHTML;
   let widget = e.path[e.path.length - 9].id;
   let date = new Date()
   let time = date.toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
 
-
-  console.log(e);
-  // axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/rfp/interactions', {
-  //   element: element,
-  //   widget: widget,
-  //   time: time
-  // }, { headers: Auth })
-  //   .then(() => {
-  //     console.log('success, interaction logged');
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   })
+  axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/rfp/interactions', {
+    element: element,
+    widget: widget,
+    time: time
+  }, { headers: Auth })
+    .then(() => {
+      console.log('success, interaction logged');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 }
